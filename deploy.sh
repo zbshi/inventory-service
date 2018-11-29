5b4cf0723247e6321f53e35d62d49f414835f22d
Junfeng Dai <ryandjf@hotmail.com>
2018-11-29 09:36:54 +0800

    fix file name

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "===Copy kube deployment file==="
-scp -o "StrictHostKeyChecking no" product-service-kube.yaml ubuntu@ec2-54-223-95-220.cn-north-1.compute.amazonaws.com.cn:/tmp/product-service-kube.yaml
+scp -o "StrictHostKeyChecking no" inventory-service-kube.yaml ubuntu@ec2-54-223-95-220.cn-north-1.compute.amazonaws.com.cn:/tmp/inventory-service-kube.yaml
 
 echo "===SSH into cluster==="
 ssh -o "StrictHostKeyChecking no" ubuntu@ec2-54-223-95-220.cn-north-1.compute.amazonaws.com.cn '
 set -e
 echo "===Deploy service and app==="
-kubectl apply -f /tmp/product-service-kube.yaml
+kubectl apply -f /tmp/inventory-service-kube.yaml
 '
